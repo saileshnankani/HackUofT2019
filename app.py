e089dcbc7e31a7d3550a47d0468d7fca2ae0b432
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-01-19 18:04:47 -0500

    Updating code

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 
 from src.api.ai import extract_image_details
+#from src.api.firebase import extract_from_image_db, query_to_expired_db
 
 app = Flask(__name__)
 
@@ -10,19 +11,30 @@ base = os.getcwd()
 ext = '/data/imgs/'
 
 broccoli = base + ext + 'broccoli/' + '0.jpg'
-#banana = base + ext + '' +'0_100.jpg'
-data = [broccoli]
+banana = base + ext + 'fruits/Training/Banana/' +'0_100.jpg'
+data = [banana, broccoli]
+
+image = data[0]
+
+def test(file):
+	files = []
+	for num, val in enumerate(data):
+		labels = extract_image_details(filename=file)
+	return f"<p> The AI has found your picture to be {labels} <p>" 
 
 @app.route('/')
 def main():
     return '<h1>Hello world!</h1>'
 
-@app.route('/detect')
-def test():
-	files = []
-#	for num, val in enumerate(data):
-	labels = extract_image_details(filename=data[0])
-	return f"<p> The AI has found your picture to be {labels} <p>" 
+@app.route('/detect', methods=['POST'])
+def image():
+	# Input: URL to DB (Firebase)
+	# Note: Json Dumps might be useful
+	#img_file = extract_from_image_bucket(img)
+    data = extract_image_details(img_file)
+    #query = query_to_expired_db(data) # in DB File
+    #send_to_user(query) # In this file
+    return "Success"
 
 if __name__ == '__main__':
     app.run(debug=True)
