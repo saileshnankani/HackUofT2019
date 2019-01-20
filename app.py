705d167b09d1f402cde53619e2eae31700e76545
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-01-20 04:22:01 -0500

    Migrating AWS code to api level and updating app.py file

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
 import os
 
+import boto3
 from flask import Flask, request
 
 from src.api.ai import extract_image_details
-#from src.api.firebase import extract_from_image_db, query_to_expired_db
+from src.api.aws import aws_lookup, query_to_expired_db
 
 app = Flask(__name__)
-
-base = os.getcwd()
-ext = '/data/imgs/'
-
-broccoli = base + ext + 'broccoli/' + '0.jpg'
-banana = base + ext + 'fruits/Training/Banana/' +'0_100.jpg'
-data = [banana, broccoli]
-
-image = data[0]
-
-def test(file):
-	files = []
-	for num, val in enumerate(data):
-		labels = extract_image_details(filename=file)
-	return f"<p> The AI has found your picture to be {labels} <p>" 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'UofTHacks2019.json' # Set's environment variables
 
 @app.route('/')
 def main():
     return '<h1>Hello world!!</h1>'
 
-@app.route('/detect', methods=['POST'])
-def image():
-	# Input: URL to DB (Firebase)
-	# Note: Json Dumps might be useful
-	#img_file = extract_from_image_bucket(img)
-    data = extract_image_details(img_file)
-    #query = query_to_expired_db(data) # in DB File
-    #send_to_user(query) # In this file
-    return "Success"
+@app.route('/detect')
+def image_analysis():
+	# Input: URL to AWS S3 image
+	# Output: Json results of the image
+
+    download_path = "1.jpg" # Testing variable (Same with 0.jpg)
+    img_file = aws_lookup(img='0.jpg', download_path=download_path) #Output: Image
+    data = extract_image_details('1.jpg') # Output: String
+    query = query_to_expired_db(data) # Output: String
+    #send_to_user(query) # Output: Json
+    return query
+
 
 if __name__ == '__main__':
     app.run(debug=True)
