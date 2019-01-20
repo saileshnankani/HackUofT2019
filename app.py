40f8fc1519a529e687893a0c96c78fd09ad729e9
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-01-20 05:41:31 -0500

    Update App File

--- app.py
+++ app.py
@@ -9,22 +9,25 @@ from src.api.aws import aws_lookup, query_to_expired_db
 app = Flask(__name__)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'UofTHacks2019.json' # Set's environment variables
 
+def general_statement(days, veggie):
+
+	# Helper function to check functionality of the backend
+	return f"This Produce Item ({veggie}) has {days} days till expiration"
+
 @app.route('/')
 def main():
     return '<h1>Hello world!!</h1>'
 
-@app.route('/detect')
+@app.route('/detect', methods=['GET','POST'])
 def image_analysis():
 	# Input: URL to AWS S3 image
 	# Output: Json results of the image
-
+	#if request.method == 'POST':
     download_path = "1.jpg" # Testing variable (Same with 0.jpg)
     img_file = aws_lookup(img='0.jpg', download_path=download_path) #Output: Image
     data = extract_image_details('1.jpg') # Output: String
     query = query_to_expired_db(data) # Output: String
-    #send_to_user(query) # Output: Json
-    return query
-
+    return general_statement(query, data) # Output: String
 
 if __name__ == '__main__':
     app.run(debug=True)
